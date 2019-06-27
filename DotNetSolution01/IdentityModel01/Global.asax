b58685b22c8e0adb8e042ff0df568722594d9a4b
Behzad Kashefipour <bkashefipour@gmail.com>
2019-06-26 17:16:31 -0700

    added working AspNetIdentity2_FromScratch

---
+++ DotNetSolution01/IdentityModel01/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="IdentityModel01.MvcApplication" Language="C#" %>
---
+++ DotNetSolution01/IdentityModel01/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(IdentityModel01.Startup))]
+namespace IdentityModel01
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
